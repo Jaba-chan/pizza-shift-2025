59c87339a1201d35c770a5b27612b280631afcf5
kuzak <kuzakovevgen@gmail.com>
2025-07-04 22:56:02 +0700

    Deleting SerialName annotations

--- feature/auth/src/main/java/ru/evgenykuzakov/auth/data/model/UserResponse.kt
+++ feature/auth/src/main/java/ru/evgenykuzakov/auth/data/model/UserResponse.kt
@@ -1,17 +1,12 @@
 package ru.evgenykuzakov.auth.data.model
 
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserResponse(
-    @SerialName("reason")
     val reason: String,
-    @SerialName("success")
     val success: Boolean,
-    @SerialName("token")
     val token: String,
-    @SerialName("user")
     val user: UserDto
 )
