7f0071db8b686b1cb20011d475ec5d84e0dea968
kuzak <kuzakovevgen@gmail.com>
2025-07-10 01:15:48 +0700

    Creating payment module

--- feature/auth/src/main/java/ru/evgenykuzakov/auth/data/mapper/UserMapper.kt
+++ feature/auth/src/main/java/ru/evgenykuzakov/auth/data/mapper/UserMapper.kt
@@ -6,8 +6,8 @@ import ru.evgenykuzakov.user.User
 fun UserDto.toDomain() = User(
     city = city,
     email = email,
-    firstname = firstname,
-    lastname = lastname,
+    firstname = firstname ?: "",
+    lastname = lastname ?: "",
     middlename = middlename,
     phone = phone
 )
